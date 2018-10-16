c8930acf042bbc4894e4e78ede8484c88bf430e1
komoriyuichiro <thekomori1113@gmail.com>
2018-10-16 15:24:03 +0900

    [add] seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+50.times do |i|
+  user = User.create(
+    name: Faker::Name.first_name,
+    created_at: Faker::Time.backward(10)
+  )
+end
